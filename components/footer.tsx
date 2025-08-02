74d947a41ddb50647ee6007783ded3feff0b0173
dvsn0 <davidsohn31@gmail.com>
2025-08-02 10:59:43 -0700

    footer and readme edits

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <p className="text-xs">
-        <span className="font-semibold">About this website:</span> Based on the original work by {" "} 
-        <a href="https://github.com/ByteGrad/portfolio-website" target="_blank"><span className="underline">ByteGrad</span></a>.
+        {/* <span className="font-semibold">About this website:</span> Based on the original work by {" "} 
+        <a href="https://github.com/ByteGrad/portfolio-website" target="_blank"><span className="underline">ByteGrad</span></a>. */}
       </p>
     </footer>
   );
