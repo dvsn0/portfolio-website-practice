afd16c3826b2c1ee329f70651e6a115d96588f8e
dvsn0 <davisohn31@gmail.com>
2025-12-23 05:29:16 -0800

    updated photo and resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import faceID from "@/public/UCSD-ID.png";
+import faceID from "@/public/chapmanphoto.jpg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
